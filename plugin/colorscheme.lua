3e17ce4aa695ba524cbb015aac15f55107de660b
AzmanBiswas <azmainbiswas007@gmail.com>
2023-07-09 23:12:37 +0530

    colorizer

--- plugin/colorscheme.lua
+++ plugin/colorscheme.lua
@@ -1,6 +1,6 @@
 require("catppuccin").setup({
     transparent_background = true,
-    styles = { 
+    styles = {
         comments = { "italic" }, -- Change the style of comments
         conditionals = {},
         loops = {},
@@ -25,26 +25,26 @@ require("tokyonight").setup({
 })
 
 require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = {
-    strings = true,
-    comments = true,
-    operators = false,
-    folds = true,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "hard", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+        strings = true,
+        comments = true,
+        operators = false,
+        folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true,    -- invert background for search, diffs, statuslines and errors
+    contrast = "hard", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = true,
 })
 
 
--- plugin/dashboard.lua
+++ plugin/dashboard.lua
@@ -3,32 +3,32 @@ local db = require('dashboard')
 local hyper = {
     theme = 'hyper',
     config = {
-      week_header = {
-       enable = true,
-      },
-      shortcut = {
-        { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-        {
-          icon = ' ',
-          icon_hl = '@variable',
-          desc = 'Files',
-          group = 'Label',
-          action = 'Telescope fd',
-          key = 'f',
+        week_header = {
+            enable = true,
         },
-        {
-          desc = ' Git Status',
-          group = 'DiagnosticHint',
-          action = 'Telescope git_status',
-          key = 'g',
+        shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+                icon = ' ',
+                icon_hl = '@variable',
+                desc = 'Files',
+                group = 'Label',
+                action = 'Telescope fd',
+                key = 'f',
+            },
+            {
+                desc = ' Git Status',
+                group = 'DiagnosticHint',
+                action = 'Telescope git_status',
+                key = 'g',
+            },
+            {
+                desc = ' open file tree',
+                group = 'Number',
+                action = 'Ex',
+                key = 'd',
+            },
         },
-        {
-          desc = ' Nvim Config',
-          group = 'Number',
-          action = 'Ex C:/Users/Azmain/AppData/Local/nvim',
-          key = 'd',
-        },
-      },
     },
 }
 
