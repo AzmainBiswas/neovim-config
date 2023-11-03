8331658cef70a3a936dc0b56c8135398ee9c473c
AzmainBiswas <azmainbiswas007@gmail.com>
2023-11-03 10:52:09 +0530

    fix telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,7 +12,7 @@ return {
     -- telescope
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.1",
+        tag = "0.1.4",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
     -- lualine
