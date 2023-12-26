a929280102bf7c1ab8e7956afdd3c03ad63042f3
Azmain Biswas <azmainbiswas007@gmail.com>
2023-12-27 01:43:05 +0530

    Wed Dec 27 01:43:05 AM IST 2023

--- plugin/colorscheme.lua
+++ plugin/colorscheme.lua
@@ -1,54 +1,53 @@
 require("catppuccin").setup({
-    transparent_background = true,
-    styles = {
-        comments = { "italic" },
-        conditionals = {},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
+	transparent_background = true,
+	styles = {
+		comments = { "italic" },
+		conditionals = {},
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+	},
 })
 
 require("tokyonight").setup({
-    style = "night",
-    transparent = true,
-    styles = {
-        comments = { italic = true },
-        functions = {}
-    },
-    sidebars = { "qf", "vista_kind", "terminal", "packer" },
+	style = "night",
+	transparent = true,
+	styles = {
+		comments = { italic = true },
+		functions = {},
+	},
+	sidebars = { "qf", "vista_kind", "terminal", "packer" },
 })
 
 require("gruvbox").setup({
-    undercurl = true,
-    underline = true,
-    bold = false,
-    italic = {
-        strings = true,
-        comments = true,
-        operators = false,
-        folds = true,
-    },
-    strikethrough = true,
-    invert_selection = false,
-    invert_signs = false,
-    invert_tabline = false,
-    invert_intend_guides = false,
-    inverse = true,    -- invert background for search, diffs, statuslines and errors
-    contrast = "hard", -- can be "hard", "soft" or empty string
-    palette_overrides = {},
-    overrides = {},
-    dim_inactive = true,
-    transparent_mode = true,
+	undercurl = true,
+	underline = true,
+	bold = false,
+	italic = {
+		strings = true,
+		comments = true,
+		operators = false,
+		folds = true,
+	},
+	strikethrough = true,
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	invert_intend_guides = false,
+	inverse = true, -- invert background for search, diffs, statuslines and errors
+	contrast = "hard", -- can be "hard", "soft" or empty string
+	palette_overrides = {},
+	overrides = {},
+	dim_inactive = true,
+	transparent_mode = true,
 })
-
 
 local color = color or "catppuccin-mocha"
 vim.cmd.colorscheme(color)
